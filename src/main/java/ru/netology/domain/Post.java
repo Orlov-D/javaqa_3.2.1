128c09dedd92ed5ff45ea653b84fb4af5257ecf4
unknown <orlovdimas92@mail.ru>
2021-06-09 00:00:53 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+public class Post {
+    private String dateTime;
+    private int id;
+    private String groupName;
+    private int groupId;
+    private boolean isSigned;
+    private String body;
+    private String urlImage;
+    private boolean isLiked;
+    private int likeCounter;
+    private boolean isReposted;
+    private int repostCounter;
+    private int looksCounter;
+
+    // + getters/setters*
+}
