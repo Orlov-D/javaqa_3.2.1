e3ea57c3f4b6255927e966b520247db69b64024f
unknown <orlovdimas92@mail.ru>
2021-06-10 23:32:47 +0300

    Little changes

---
+++ src/main/java/ru/netology/domain/Comment.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class Comment {
+    private int count;
+    private boolean canPost;
+    private String body;
+
+    // + get/set на все поля
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,18 +1,23 @@
 package ru.netology.domain;
 
 public class Post {
-    private String dateTime;
     private int id;
-    private String groupName;
-    private int groupId;
-    private boolean isSigned;
+    private int ownerId;
+    private String authorName;
+    private int postedBy;
+    //Во намутили. И группы, и админы, и платникам и прочее прочее... половины не понятно.
+    //Где и как это используется и зачем...
+    private String dateTime;
     private String body;
-    private String urlImage;
     private boolean isLiked;
     private int likeCounter;
     private boolean isReposted;
     private int repostCounter;
     private int looksCounter;
+    private String groupName;
+    private int groupId;
+    private boolean isSigned;
+    private boolean isAdvertising;
 
     // + getters/setters*
 }
