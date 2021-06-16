fdb70b94cda407e0c29eb039141b9f7fda48b832
Orlov-D <82273356+Orlov-D@users.noreply.github.com>
2021-06-16 21:56:05 +0300

    Осознание пришло
    Мы создаем класс, передавать что то конкретное мы будем позже. А именно в тестовом классе...

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,13 +5,6 @@ import ru.netology.domain.Post;
 public class PostManager {
 
     private Post[] post;
-//    Post poster = new Post [] post;
-//    Post rest = new Post();
-
-
-//    public Post[] getPost() {
-//        return post;
-//    }
 
     public Post[] searchPost(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
 //        TODO: add logic
